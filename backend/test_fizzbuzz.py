be6dd3f199d513b02042496e260eb9291388fdbc
testgt2/polecat <polecat@testgt2.local>
2026-05-15 17:48:56 +0200

    Implement backend fizzbuzz

--- backend/test_fizzbuzz.py
+++ backend/test_fizzbuzz.py
@@ -1,12 +1,15 @@
 import unittest
-from .fizzbuzz import fizzbuzz
+from backend.fizzbuzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_fizzbuzz(self):
-        self.assertEqual(fizzbuzz(1), "1")
-        self.assertEqual(fizzbuzz(3), "Fizz")
-        self.assertEqual(fizzbuzz(5), "Buzz")
+    def test_15(self):
         self.assertEqual(fizzbuzz(15), "FizzBuzz")
+    def test_3(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+    def test_5(self):
+        self.assertEqual(fizzbuzz(5), "Buzz")
+    def test_default(self):
+        self.assertEqual(fizzbuzz(1), "1")
 
 if __name__ == "__main__":
     unittest.main()
