d2d477d8c74d2d16ade54348314e4b77bdd3b0c1
testgt2/polecat <polecat@testgt2.local>
2026-05-15 17:45:22 +0200

    Implement te-a03

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,2 @@
-#!/usr/bin/env python3
-from fizzbuzz import fizzbuzz
-
-def main():
-    for i in range(1, 101):
-        print(fizzbuzz(i))
-
 if __name__ == "__main__":
-    main()
+    print("Hello from main")
