cf5e456477fd5729d45854484c2547ca75cb1a5f
testgt2/polecat <polecat@testgt2.local>
2026-05-15 17:43:51 +0200

    Implement te-abc

---
+++ backend/main.py
@@ -0,0 +1,5 @@
+from .fizzbuzz import fizzbuzz
+
+def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
--- backend/test_fizzbuzz.py
+++ backend/test_fizzbuzz.py
@@ -1,5 +1,9 @@
 import unittest
+from .fizzbuzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
     def test_fizzbuzz(self):
-        self.assertTrue(True)
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(fizzbuzz(3), "Fizz")
+        self.assertEqual(fizzbuzz(5), "Buzz")
+        self.assertEqual(fizzbuzz(4), "4")
