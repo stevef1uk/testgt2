f99a90b65a20ce9da2d8708c17181a81c7e97987
testgt2/polecat <polecat@testgt2.local>
2026-05-15 17:13:47 +0200

    Implement te-jf4

--- backend/test_fizzbuzz.py
+++ backend/test_fizzbuzz.py
@@ -2,11 +2,8 @@ import unittest
 from backend.fizzbuzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_sample(self):
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
         self.assertEqual(fizzbuzz(3), "Fizz")
         self.assertEqual(fizzbuzz(5), "Buzz")
-        self.assertEqual(fizzbuzz(15), "FizzBuzz")
         self.assertEqual(fizzbuzz(1), "1")
-
-if __name__ == "__main__":
-    unittest.main()
