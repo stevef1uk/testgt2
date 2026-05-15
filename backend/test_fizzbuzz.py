f4f8dbf139230edd1e601b189868a9905102f81c
testgt2/polecat <polecat@testgt2.local>
2026-05-15 17:44:22 +0200

    Implement te-11w

--- backend/test_fizzbuzz.py
+++ backend/test_fizzbuzz.py
@@ -1,9 +1,12 @@
 import unittest
-from .fizzbuzz import fizzbuzz
+from fizzbuzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
     def test_fizzbuzz(self):
-        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(fizzbuzz(1), "1")
         self.assertEqual(fizzbuzz(3), "Fizz")
         self.assertEqual(fizzbuzz(5), "Buzz")
-        self.assertEqual(fizzbuzz(4), "4")
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+
+if __name__ == "__main__":
+    unittest.main()
