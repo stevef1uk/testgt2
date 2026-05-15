e6356c655cafdb9c50285244a9b27dd175d2e23a
testgt2/polecat <polecat@testgt2.local>
2026-05-15 17:44:30 +0200

    Implement te-y3z

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
-from .fizzbuzz import fizzbuzz
+#!/usr/bin/env python3
+from fizzbuzz import fizzbuzz
 
 def main():
     for i in range(1, 101):
         print(fizzbuzz(i))
+
+if __name__ == "__main__":
+    main()
